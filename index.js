60ca7b020a28011408f0445b42e40b028e709335
Dattatri <dattatribachare9764@gmail.com>
2024-06-21 16:21:43 +0530

    js file added

---
+++ index.js
@@ -0,0 +1,7 @@
+x = Number(prompt('The first number is : '));
+y = Number(prompt('<br>The Second number is : '));
+document.write("<b>The first number is : <b>",x)
+document.write("<b><br>The Second number is : <b>",y)
+document.write("<b><br> Sum = <b>",x + y);
+
+document.write("<br><br><br>");
