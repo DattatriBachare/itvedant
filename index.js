c3b46504b66ab13a792d19a39d4456f88a43aed7
Dattatri <dattatribachare9764@gmail.com>
2024-06-26 15:12:48 +0530

    added tempruary files

--- index.js
+++ index.js
@@ -3,5 +3,15 @@ y = Number(prompt('<br>The Second number is : '));
 document.write("<b>The first number is : <b>",x)
 document.write("<b><br>The Second number is : <b>",y)
 document.write("<b><br> Sum = <b>",x + y);
-
 document.write("<br><br><br>");
+// 
+
+let heat = false
+if (heat == true)
+    {
+        document.write("Switch on the AC");
+    }
+else
+    {
+        document.write("Don't switch on the AC");
+    }
---
+++ temp.js
@@ -0,0 +1,9 @@
+let toll_amount_paid = true
+if(toll_amount_paid == true)
+    {
+        document.write("<b> Let vehical to go <b>");
+    }
+else
+    {
+        document.write("Stop the vehical");
+    }
