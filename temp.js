33ba2c5945a6edb0978956e9379e63d37f6b5a98
Dattatri <dattatribachare9764@gmail.com>
2024-06-27 07:23:36 +0530

    some extra coding is added

--- temp.js
+++ temp.js
@@ -1,4 +1,4 @@
-let toll_amount_paid = true
+/* let toll_amount_paid = true
 if(toll_amount_paid == true)
     {
         document.write("<b> Let vehical to go <b>");
@@ -7,3 +7,37 @@ else
     {
         document.write("Stop the vehical");
     }
+*/
+/*
+ x = prompt("Enter value of x : ");
+ y = prompt("Enter value of y : ");
+ x = parseFloat(x);
+ y = parseFloat(y);
+window.alert("The sum of x and y is : " + (x + y))*/
+
+alert("Welcome to RTO");
+
+let input = parseInt(prompt("Type 1. for light Licence \n        2. for heavy Licence : "));
+
+if (input == 1){
+    let age = parseInt(prompt("Enter age : "));
+    if (age >= 18){
+        alert("You are eligible for light Licence ..!");
+    }
+    else{
+        alert("You are not eligible.");
+    }
+}
+else if (input == 2){
+    let age = parseInt(prompt("Enter your age : "));
+    if (age > 21){
+        alert("You are eligible ..!");
+    }
+    else{
+        alert("Wait for "+(21 - age)+" more years");
+    }
+}
+else{
+    alert("Enter a valid input")
+}
+console.log("Hello world")
